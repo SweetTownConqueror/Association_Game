5afb28f75279fbb5a90e79253d517904ec1e8cd3
SweetTownConqueror <41520148+SweetTownConqueror@users.noreply.github.com>
2019-04-07 15:00:18 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Assiociation_Game
+    Private form As Integer
+    Private colour As Integer
+    Private goodAnswer As Integer = 0
+    Private wrongAnswer As Integer = 0
+    Private nbTry As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Start()
+        Timer1.Interval = 1000
+        Dim timer As New Stopwatch
+        timer.Start()
+        Label1.ForeColor = Color.Black
+        Label1.Text = Val(Label1.Text) - 1
+        If Label1.Text < 60 Then
+            Label1.ForeColor = Color.Red
+            If Label1.Text <= 0 Then
+                Timer1.Enabled = False
+                MsgBox("Time's up! Résultat : " + goodAnswer.ToString() + "/" + (goodAnswer + wrongAnswer).ToString() + " soit " + ((goodAnswer * 100) / (goodAnswer + wrongAnswer)).ToString("00.00") + "% de bonnes réponses")
+            End If
+        End If
+        ' Initialize the random-number generator.
+        Randomize()
+        ' Generate random value between 1 and 4.
+        form = CInt(Int((3 * Rnd()) + 1))
+        colour = CInt(Int((4 * Rnd()) + 1))
+        While timer.ElapsedMilliseconds < 500
+
+            Dim MyGraphics As Graphics = Me.CreateGraphics
+            Dim redBrush As SolidBrush = New SolidBrush(Color.Red)
+            Dim greenBrush As SolidBrush = New SolidBrush(Color.LawnGreen)
+            Dim blueBrush As SolidBrush = New SolidBrush(Color.Blue)
+            Dim yellowBrush As SolidBrush = New SolidBrush(Color.Yellow)
+            If form = 1 Then
+                If colour = 1 Then
+                    MyGraphics.FillEllipse(redBrush, 80, 80, 70, 70)
+                ElseIf colour = 2 Then
+                    MyGraphics.FillEllipse(greenBrush, 80, 80, 70, 70)
+                ElseIf colour = 3 Then
+                    MyGraphics.FillEllipse(blueBrush, 80, 80, 70, 70)
+                ElseIf colour = 4 Then
+                    MyGraphics.FillEllipse(yellowBrush, 80, 80, 70, 70)
+                End If
+            ElseIf form = 2 Then
+                If colour = 1 Then
+                    MyGraphics.FillRectangle(redBrush, 80, 80, 70, 70)
+                ElseIf colour = 2 Then
+                    MyGraphics.FillRectangle(greenBrush, 80, 80, 70, 70)
+                ElseIf colour = 3 Then
+                    MyGraphics.FillRectangle(blueBrush, 80, 80, 70, 70)
+                ElseIf colour = 4 Then
+                    MyGraphics.FillRectangle(yellowBrush, 80, 80, 70, 70)
+                End If
+            ElseIf form = 3 Then
+                If colour = 1 Then
+                    Dim ptsArray As PointF() = {New PointF(160.0F, 120.0F), New PointF(110.0F, 80.0F), New PointF(60.0F, 120.0F), New PointF(110.0F, 160.0F)}
+                    Dim gp As New Drawing2D.GraphicsPath(Drawing2D.FillMode.Alternate)
+                    gp.AddLines(ptsArray)
+                    gp.CloseFigure()
+                    MyGraphics.FillPath(redBrush, gp)
+                    'MyGraphics.DrawLines(Pens.Black, ptsArray)
+                    'MyGraphics.FillEllipse(redBrush, 80, 80, 70, 70)
+                ElseIf colour = 2 Then
+                    Dim ptsArray As PointF() = {New PointF(160.0F, 120.0F), New PointF(110.0F, 80.0F), New PointF(60.0F, 120.0F), New PointF(110.0F, 160.0F)}
+                    Dim gp As New Drawing2D.GraphicsPath(Drawing2D.FillMode.Alternate)
+                    gp.AddLines(ptsArray)
+                    gp.CloseFigure()
+                    MyGraphics.FillPath(greenBrush, gp)
+                    'MyGraphics.FillEllipse(greenBrush, 80, 80, 70, 70)
+                ElseIf colour = 3 Then
+                    Dim ptsArray As PointF() = {New PointF(160.0F, 120.0F), New PointF(110.0F, 80.0F), New PointF(60.0F, 120.0F), New PointF(110.0F, 160.0F)}
+                    Dim gp As New Drawing2D.GraphicsPath(Drawing2D.FillMode.Alternate)
+                    gp.AddLines(ptsArray)
+                    gp.CloseFigure()
+                    MyGraphics.FillPath(blueBrush, gp)
+                    'MyGraphics.FillEllipse(blueBrush, 80, 80, 70, 70)
+                ElseIf colour = 4 Then
+                    Dim ptsArray As PointF() = {New PointF(160.0F, 120.0F), New PointF(110.0F, 80.0F), New PointF(60.0F, 120.0F), New PointF(110.0F, 160.0F)}
+                    Dim gp As New Drawing2D.GraphicsPath(Drawing2D.FillMode.Alternate)
+                    gp.AddLines(ptsArray)
+                    gp.CloseFigure()
+                    MyGraphics.FillPath(yellowBrush, gp)
+                    'MyGraphics.FillEllipse(yellowBrush, 80, 80, 70, 70)
+                End If
+            End If
+            't2
+        End While
+        Me.Invalidate()
+        nbTry = nbTry + 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Timer1.Enabled = True Then
+            Timer1.Start()
+            'Form1_Load()
+        Else
+            Label1.Text = 360
+            Timer1.Start()
+        End If
+    End Sub
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, ByVal keyData As Keys) As Boolean
+        Dim bHandled As Boolean = False
+        If keyData = Keys.Left Then
+            bHandled = True
+            If form = 1 And (colour <> 1) Then
+                'Label2.Text = "bonne forme rond ! "
+                goodAnswer = goodAnswer + 1
+            Else
+                If nbTry < 90 Then
+                    Timer1.Stop()
+                    MsgBox("Raté. Rappel : gauche : rond vert bleu jaune")
+                    If MsgBoxResult.Ok Then
+                        Timer1.Start()
+                    End If
+                End If
+                wrongAnswer = wrongAnswer + 1
+            End If
+        ElseIf keyData = Keys.Down Then
+            bHandled = True
+            If form = 2 And (colour <> 2) Then
+                'Label2.Text = "bonne forme carre ! "
+                goodAnswer = goodAnswer + 1
+            Else
+                If nbTry < 90 Then
+                    Timer1.Stop()
+                    MsgBox("Raté. Rappel : bas : carre rouge bleu jaune")
+                    If MsgBoxResult.Ok Then
+                        Timer1.Start()
+                    End If
+                End If
+                wrongAnswer = wrongAnswer + 1
+            End If
+        ElseIf keyData = Keys.Right Then
+            bHandled = True
+            If form = 3 And (colour <> 3) Then
+                'Label2.Text = "bonne forme losange ! "
+                goodAnswer = goodAnswer + 1
+            Else
+                If nbTry < 90 Then
+                    Timer1.Stop()
+                    MsgBox("Raté. rappel : droite : losange rouge vert jaune")
+                    If MsgBoxResult.Ok Then
+                        Timer1.Start()
+                    End If
+                End If
+                wrongAnswer = wrongAnswer + 1
+            End If
+        End If
+        Return bHandled
+    End Function
+End Class
---
+++ README.txt
@@ -0,0 +1,53 @@
+--------------------------------------------------------------------------------------------------------------------------
+					FRANCAIS
+--------------------------------------------------------------------------------------------------------------------------
+
+
+Dans certains tests psychotechnique pour les recrutements dans le milieu de l'aéronautique, il y a une épreuve 
+d'association qui permet d'évaluer les capacités des candidats à associer une forme a une couleur, en un laps de temps relativement
+restreint, le tout durant une période de temps qui peût être relativement longue, afin de tester l'attention et la capacité de
+concentration du candidat dans le temps.
+
+Ceci est mon premier programme VisualBasic, je l'ai développé sous le framework .net avec l'IDE VisualStudio.
+Au delà du fait qu'il m'a donné un premier apercu de la programmation en VB, il peut également servir de base si un jour vous comptez
+passer ce type de sélections.
+
+
+Règles du Jeu :
+
+Durant 6 minutes vous aurez des formes qui aparaîtront.
+- Si un rond apparaît, vous devez appuyer sur la touche gauche de votre clavier sauf si c'est un rond rouge auquel cas vous ne devez appuyer sur aucune touche
+- Si un carré apparaît, vous devez appuyer sur la touche bas de votre clavier sauf si c'est un carré vert auquel cas vous ne devez appuyer sur aucune touche
+- Si un losange apparaît, vous devez appuyer sur la touche droitede votre clavier sauf si c'est un losange bleu auquel cas vous ne devez appuyer sur aucune touche
+Une session dure 6 minutes mais lors de vos sélections cela pourra aller jusque 18 minutes, vous pouvez donc faire 3 sessions d'affilées.
+Durant une minute 30 si vous vous trompez une boite de dialogue vous dira que vous vous etes trompé et vous rappellera les règles.
+Souvenez vous bien ces commandes car au dela de ce temps vous n'aurez plus d'aide!
+Bon entrainement / Bon jeu !
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+					ENGLISH
+--------------------------------------------------------------------------------------------------------------------------
+
+In some psychotechnics tests for aeronautic recruitment, there may have a psychotechnic association test which allow to the company
+to evaluate the ability of the candidate to associate a form with a colour, in a relatively short amount of time.
+The test may last a period of time long enough to evaluate the attention and the ability of the candidate to stay focused.
+
+This is my first programm in VisualBasic, I developped it under the framework .net, with the IDE Visual Studio.
+Beyond the fact that it gave me a first experience with VB' programmation, it also may help you if you are to go through these kind of tests
+
+
+Rules of the game :
+
+During 6 minutes, some forms will appear.
+- If a circle appear, push left, except if it's a red circle, then you don't have to push any keys
+- If a square appear, push down, except if it's a green square, then you don't have to push any keys
+- If a diamond appear, push right, except if it's a blue diamond, then you don't have to push any keys
+One session last 6 minutes, but during the selections i may last until up to 18 minutes, so I advise you to play 3 session in a row to get closest as real conditions.
+During 1 minute 30 if you get wrong, a user prompt will appear telling you that youre wrong, and will remind you the rules.
+Try to Remember the rules because beyond this time there will be no more help!
+Have a nice Training / Gaming !
